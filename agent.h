ca620671f49a50b2c69c610bdbd0a46206fc1bad
nax1016cs <51845490+nax1016cs@users.noreply.github.com>
2019-10-23 20:43:48 +0800

    Add files via upload

---
+++ agent.h
@@ -0,0 +1,151 @@
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <set>
+#include <type_traits>
+#include <algorithm>
+#include "board.h"
+#include "action.h"
+
+int act =0;
+class agent {
+public:
+	agent(const std::string& args = "") {
+		std::stringstream ss("name=unknown role=unknown " + args);
+		for (std::string pair; ss >> pair; ) {
+			std::string key = pair.substr(0, pair.find('='));
+			std::string value = pair.substr(pair.find('=') + 1);
+			meta[key] = { value };
+		}
+	}
+	virtual ~agent() {}
+	virtual void open_episode(const std::string& flag = "") {}
+	virtual void close_episode(const std::string& flag = "") {}
+	virtual action take_action(const board& b) { return action(); }
+	virtual bool check_for_win(const board& b) { return false; }
+public:
+	virtual std::string property(const std::string& key) const { return meta.at(key); }
+	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+	virtual std::string name() const { return property("name"); }
+	virtual std::string role() const { return property("role"); }
+
+protected:
+	typedef std::string key;
+	struct value {
+		std::string value;
+		operator std::string() const { return value; }
+		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+		operator numeric() const { return numeric(std::stod(value)); }
+	};
+	std::map<key, value> meta;
+
+};
+
+class random_agent : public agent {
+public:
+	random_agent(const std::string& args = "") : agent(args) {
+		if (meta.find("seed") != meta.end())
+			engine.seed(int(meta["seed"]));
+	}
+	virtual ~random_agent() {}
+
+protected:
+	std::default_random_engine engine;
+};
+
+/**
+ * random environment
+ * add a new random tile to an empty cell
+ * 2-tile: 90%
+ * 4-tile: 10%
+ */
+class rndenv : public random_agent {
+public:
+	virtual void close_episode(const std::string& flag = "") {ct=0;choose =3;};
+	rndenv(const std::string& args = "") : random_agent("name=random role=environment " + args),
+		space({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }),ct(0),bag({1,2,3}), popup(0, 9) {}
+	virtual action take_action(const board& after) {
+		if(ct<9){
+			if(choose >2){
+				choose = 0;
+				std::shuffle(bag.begin(), bag.end(), engine);
+			}
+			board::cell tile = bag[choose++];
+			std::shuffle(space.begin(), space.end(), engine);
+			for (int pos : space) {
+				if (after(pos) != 0) {
+					continue;
+				}
+				else{
+					ct++;
+					return action::place(pos, tile);
+				}
+			}
+			return action();
+		}
+		
+		else{
+			if(choose >2){
+				choose = 0;
+				std::shuffle(bag.begin(), bag.end(), engine);
+			}
+			board::cell tile = bag[choose++];
+			std::shuffle(sel_pos[act].begin(), sel_pos[act].end() ,engine);
+			for (int pos =0; pos < 4; pos++) {
+				if (after(sel_pos[act][pos]) != 0) continue;
+				return action::place(sel_pos[act][pos], tile);
+			}
+			return action();
+		}	
+		
+			
+	}
+
+private:
+	std::array<int, 16> space;
+	std::uniform_int_distribution<int> popup;
+	int last_action;
+	std::array<int, 3> bag;
+	int choose = 3;
+	int ct ;
+	std::array<std::array<int, 4> ,4> sel_pos = {{
+		{{12,13,14,15}},
+
+		{{0,4,8,12}},
+		
+		{{0,1,2,3}},
+
+		{{3,7,11,15}}
+	}};
+};
+
+/**
+ * dummy player
+ * select a legal action randomly
+ */
+class player : public random_agent {
+public:
+	player(const std::string& args = "") : random_agent("name=dummy role=player " + args),
+		opcode({ 0, 1, 2, 3 }) {}
+
+	virtual action take_action(const board& before) {
+		std::shuffle(opcode.begin(), opcode.end(), engine);
+		int best_op =0;
+		int temp = -1;
+		for (int op : opcode) {
+			board::reward reward = board(before).slide(op);
+			if(reward>temp){
+				temp = reward;
+				best_op = op;
+			}
+		}
+		act = best_op;
+		return action::slide(best_op);
+		return action();
+	}
+
+private:
+	std::array<int, 4> opcode;
+};
